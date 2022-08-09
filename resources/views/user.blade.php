8a10aa8f311b8de218b76d230a786aaed8406e4c
khalil el kadih <khalil94elkadih@gmail.com>
2022-08-09 19:01:50 +0200

    create basic route

---
+++ resources/views/user.blade.php
@@ -0,0 +1,7 @@
+<?php
+
+
+echo '<h1>hello from user </h1>';
+
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//create basic route
+Route::get('user',function()
+{
+
+    return view('user');
+    //return 'user';
+}
+);
